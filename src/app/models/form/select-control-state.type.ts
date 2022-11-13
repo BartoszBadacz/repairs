267f34d0a9caa2cae86ad878806836b0b058f3fd
Bartek <badacz.bartosz@gmail.com>
2022-11-13 23:42:45 +0100

    poprwki w formularzach

--- src/app/models/form/select-control-state.type.ts
+++ src/app/models/form/select-control-state.type.ts
@@ -2,5 +2,5 @@ import {InputBaseState} from "./input-base-state.type";
 import {SelectControlOption} from "./select-control-option.type";
 
 export interface SelectControlState extends InputBaseState {
-options: SelectControlOption[];
+  options: SelectControlOption[];
 }
